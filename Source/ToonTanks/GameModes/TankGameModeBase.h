806f8e53cd5c5bc10b0c11d78e2072f2b00d7f2b
Denzel Nasol <59032895+denzelnasol@users.noreply.github.com>
2020-09-03 20:51:23 -0700

    ActorDied function added to TankGameModeBase

--- Source/ToonTanks/GameModes/TankGameModeBase.h
+++ Source/ToonTanks/GameModes/TankGameModeBase.h
@@ -6,28 +6,30 @@
 #include "GameFramework/GameModeBase.h"
 #include "TankGameModeBase.generated.h"
 
-/**
- * 
- */
+class APawnTurret;
+class APawnTank;
+
 UCLASS()
 class TOONTANKS_API ATankGameModeBase : public AGameModeBase
 {
 	GENERATED_BODY()
 	
 private:
+	APawnTank* PlayerTank;
+	int32 TargetTurrets = 0;
 
+	int32 GetTargetTurretCount();
 	void HandleGameStart();
 	void HandleGameOver(bool PlayerWon);
 
 public:
-
 	void ActorDied(AActor* DeadActor);
 
 protected:
 
 	virtual void BeginPlay() override;
 	UFUNCTION(BlueprintImplementableEvent)	
-	void GamerStart();
+	void GameStart();
 	UFUNCTION(BlueprintImplementableEvent)
 	void GameOver(bool PlayerWon);
 };
